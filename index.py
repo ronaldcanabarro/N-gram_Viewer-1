d6be96a1b27e65bdfe1c9f4faf1671140f2a027b
Wemy Vieira Felipe <wemyfelype@gmail.com>
2023-05-23 08:55:29 -0300

    add readme.md

--- index.py
+++ index.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, jsonify, request
 from service import processar
 import json
 app = Flask(__name__)
+import sqlite3
 
+sqlite3.connect("teste.db")
 
 @app.route('/')
 def hello_world():  # put application's code here
